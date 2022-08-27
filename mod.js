c0d64228d29b256e445e766c20807c23dd0937e9
1NF35TAT10N <leeyuzhe09@gmail.com>
2022-08-27 14:20:13 +0800

    yep
    yep?

--- mod.js
+++ mod.js
@@ -1,7 +1,7 @@
 G.AddData({
-    name:'Monument',
-    author:'Inf09',
-    desc:'A mod that expands upon the core gameplay loop of Orteils Neverending Legacy.',
+    name:'Example mod',
+    author:'Orteil',
+    desc:'A simple example mod that adds hot peppers and hot sauce.',
     engineVersion:1,
     manifest:'modManifest.js',
     requires:['Default dataset*'],
@@ -12,7 +12,7 @@ G.AddData({
         
         //First we create a couple new resources :
         new G.Res({
-            name:'Chili',
+            name:'hot pepper',
             desc:'[hot pepper]s are loaded with capsaicin and, depending on who you ask, may produce a pleasant burn when eaten.',
             icon:[0,0,'spicySheet'],
             turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
